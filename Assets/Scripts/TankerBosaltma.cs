28d34880a26ff67d00387d50a030a286f38ebf1b
İrfan Yiğit Soğan <168006634+DamaneX0@users.noreply.github.com>
2024-07-30 23:47:06 +0300

    TankerinAyarlamalarıYapıldı

--- Assets/Scripts/TankerBosaltma.cs
+++ Assets/Scripts/TankerBosaltma.cs
@@ -2,33 +2,32 @@ using UnityEngine;
 
 public class WaterFillController : MonoBehaviour
 {
-    public GameObject waterObject; // Su nesnesi
-    [Range(0, 1)]
-    public float fillAmount = 1.0f; // 0 ile 1 arasýnda bir deðer (1 = tam dolu, 0 = boþ)
+        
+        public GameObject waterObject; // Su nesnesi
+        [Range(0, 1)]
+        public float fillAmount = 1.0f; // 0 ile 1 arasýnda bir deðer (1 = tam dolu, 0 = boþ)
 
-    private Vector3 initialScale;
-    private Vector3 initialPosition;
+        private Vector3 initialScale;
+       
 
-    void Start()
-    {
-        if (waterObject != null)
+        void Start()
         {
-            initialScale = waterObject.transform.localScale;
-            initialPosition = waterObject.transform.localPosition;
+            if (waterObject != null)
+            {
+                initialScale = waterObject.transform.localScale;
+              
+            }
+        }
+
+        void Update()
+        {
+            if (waterObject != null)
+            {
+                // fillAmount'a göre su nesnesinin yüksekliðini ayarla
+                waterObject.transform.localScale = new Vector3(initialScale.x, initialScale.y , initialScale.z * fillAmount);
+
+            }
         }
     }
 
-    void Update()
-    {
-        if (waterObject != null)
-        {
-            // fillAmount'a göre su nesnesinin yüksekliðini ayarla
-            waterObject.transform.localScale = new Vector3(initialScale.x, initialScale.y, initialScale.z * fillAmount);
-
-            // fillAmount'a göre su nesnesinin pozisyonunu ayarla ki su yukarýdan azalsýn
-            float adjustedY = initialPosition.y - (initialScale.y * (1 - fillAmount) / 2);
-            waterObject.transform.localPosition = new Vector3(initialPosition.x, adjustedY, initialPosition.z);
-        }
-    }
-}
 
